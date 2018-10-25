32fa5ced06b81bb9e3a4ce7ea798c6770b424cc3
Kerr1Gan <674712964@qq.com>
2018-10-25 22:47:23 +0800

    dev

--- smsservice/src/main/java/com/starwin/smsservice/SmsServiceApplication.java
+++ smsservice/src/main/java/com/starwin/smsservice/SmsServiceApplication.java
@@ -2,11 +2,12 @@ package com.starwin.smsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SmsServiceApplication {
+public class SmsServiceApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SmsServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SmsServiceApplication.class, args);
+    }
 }
