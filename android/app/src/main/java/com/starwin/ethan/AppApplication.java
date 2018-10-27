cdc537bcb6c7e25816c9d88bbe8c7871d976573a
Kerr1Gan <674712964@qq.com>
2018-10-27 15:52:16 +0800

    dev

--- android/app/src/main/java/com/starwin/ethan/AppApplication.java
+++ android/app/src/main/java/com/starwin/ethan/AppApplication.java
@@ -2,5 +2,15 @@ package com.starwin.ethan;
 
 import android.app.Application;
 
+import com.starwin.ethan.smsservice.BuildConfig;
+import com.tencent.bugly.crashreport.CrashReport;
+
 public class AppApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        CrashReport.initCrashReport(getApplicationContext(), "3945c078d0", BuildConfig.DEBUG);
+    }
 }
