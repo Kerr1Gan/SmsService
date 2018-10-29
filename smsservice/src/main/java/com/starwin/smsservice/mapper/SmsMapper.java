5bfd18163d53b0820775902e394d66fc7f09545d
Kerr1Gan <674712964@qq.com>
2018-10-29 10:13:59 +0800

    dev

--- smsservice/src/main/java/com/starwin/smsservice/mapper/SmsMapper.java
+++ smsservice/src/main/java/com/starwin/smsservice/mapper/SmsMapper.java
@@ -10,13 +10,13 @@ import java.util.List;
 @Component
 public interface SmsMapper {
 
-    @Select("select * from table_sms")
+    @Select("select * from table_sms order by id desc")
     List<SmsMessage> getAllSms();
 
-    @Select("select * from table_sms limit ${index},${length}")
+    @Select("select * from table_sms  order by id desc limit ${index},${length}")
     List<SmsMessage> getSmsByIndex(@Param(value = "index") int index, @Param(value = "length") int length);
 
-    @Select("select * from table_sms where self_phone =#{selfPhone} limit ${index},${length}")
+    @Select("select * from table_sms where self_phone =#{selfPhone}  order by id desc limit ${index},${length}")
     List<SmsMessage> getSmsByIndexAndPhone(@Param(value = "index") int index, @Param(value = "length") int length, @Param(value = "selfPhone") String selfPhone);
 
     @Insert({"insert into table_sms(phone, type, content,date,self_phone) values(#{phone}, #{type}, #{content}, #{date},#{selfPhone})"})
