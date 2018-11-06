d88f96de1d4c0acbc62d59497e8c7d829d8bae2e
Kerr1Gan <674712964@qq.com>
2018-11-06 16:53:52 +0800

    dev

--- smsservice/src/main/java/com/starwin/smsservice/SmsServiceApplication.java
+++ smsservice/src/main/java/com/starwin/smsservice/SmsServiceApplication.java
@@ -3,8 +3,10 @@ package com.starwin.smsservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling   //开启定时任务注解
 public class SmsServiceApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
